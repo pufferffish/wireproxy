bc77be08cc279dabe94f491ec79e8c2c13b020c5
octeep <octeep@pm.me>
2022-08-23 21:56:29 +0800

    bump version to 1.0.4

--- cmd/wireproxy/main.go
+++ cmd/wireproxy/main.go
@@ -14,7 +14,7 @@ import (
 // an argument to denote that this process was spawned by -d
 const daemonProcess = "daemon-process"
 
-var version = "dev"
+var version = "1.0.4-dev"
 
 // attempts to pledge and panic if it fails
 // this does nothing on non-OpenBSD systems
