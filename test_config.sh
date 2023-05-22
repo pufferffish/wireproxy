df9cb055735e8c2c87aace5a84194472763fa85e
octeep <github@bandersnatch.anonaddy.com>
2023-05-22 16:37:10 +0100

    add test CI

---
+++ test_config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+exec 3<>/dev/tcp/demo.wireguard.com/42912
+privatekey="$(wg genkey)"
+wg pubkey <<<"$privatekey" >&3
+IFS=: read -r status server_pubkey server_port internal_ip <&3
+[[ $status == OK ]]
+cat >test.conf <<EOL
+[Interface]
+Address = $internal_ip/32
+PrivateKey = $privatekey
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = $server_pubkey
+Endpoint = demo.wireguard.com:$server_port
+
+[Socks5]
+BindAddress = 127.0.0.1:64423
+EOL
